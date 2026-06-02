acb1c460a7a1868e0a106daab890937b1a2af29a
kaeforwin <kaeforwin@gmail.com>
2026-06-02 14:19:57 +0300

    Add glowing cursor effect on mouse movement

---
+++ script.js
@@ -0,0 +1,13 @@
+const glow = document.createElement("div");
+
+glow.classList.add("cursor-glow");
+
+document.body.appendChild(glow);
+
+document.addEventListener("mousemove",(e)=>{
+
+glow.style.left=e.clientX+"px";
+
+glow.style.top=e.clientY+"px";
+
+});
